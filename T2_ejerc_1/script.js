15ee49ce4e42088e40f9bc1f67cc871a6570e27f
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-04 11:52:41 +0100

    Comienzo

---
+++ T2_ejerc_1/script.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Introduzca nombre");
+let apellidos = prompt("Introduzca apellidos");
+let direccion = prompt("Introduzca dirección");
+let telefono = prompt("Introduzca telefono");
+let dni = prompt("Introduzca DNI");
