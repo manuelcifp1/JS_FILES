3825f54d613b0887c5e48928013ff268f04282f5
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-05 09:34:28 +0100

    T2_ejercicio_1

--- T2_ejerc_1/script.js
+++ T2_ejerc_1/script.js
@@ -3,3 +3,11 @@ let apellidos = prompt("Introduzca apellidos");
 let direccion = prompt("Introduzca dirección");
 let telefono = prompt("Introduzca telefono");
 let dni = prompt("Introduzca DNI");
+
+alert(`Estos son sus datos de contacto: ${nombre} ${apellidos} ${direccion} ${telefono} ${dni}`);
+
+document.write(`Estos son sus datos de contacto: ${nombre} ${apellidos} ${direccion} ${telefono} ${dni}`);
+
+document.getElementById("parrafo").innerHTML = `Estos son sus datos de contacto: ${nombre} ${apellidos} ${direccion} ${telefono} ${dni}`;
+
+console.log(`Estos son sus datos de contacto: ${nombre} ${apellidos} ${direccion} ${telefono} ${dni}`);
