812f58fc4c5056fe9d77f03f84bba3fb82448ce9
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-15 11:34:34 +0100

    se acabó

--- PRÁCTICAS/TEMA 2/P6_T2/script.js
+++ PRÁCTICAS/TEMA 2/P6_T2/script.js
@@ -10,13 +10,14 @@ function numeroAleatorio() {
 
 //Asignamos a una variable la llamada de la función anterior.
 let returnAleatorio = numeroAleatorio();
+console.log(returnAleatorio);
 
 //Creamos otra función para la mecánica del juego.
-function empezarJuego() {    
+function empezarJuego() {     
 
     let numeroUsuario = prompt("Dame un numero entre 1 y 1000");
     if (numeroUsuario == null) {
-        alert("El juego se ha cancelado.");
+        document.write("<h2>El juego se ha cancelado.</h2>");
     } else {
         intentos++;
     numeroUsuario = Number(numeroUsuario);    
@@ -32,15 +33,23 @@ function empezarJuego() {
         return empezarJuego();
     } else {
         if (numeroUsuario == returnAleatorio) {            
-            document.write("<h2>¡Enhorabuena! Lo has adivinado en " + intentos + "intentos.</h2>");
-            let jugarDeNuevo = confirm("¿Quieres jugar de nuevo?");
-        if (jugarDeNuevo) {
-            intentos = 0;
-            returnAleatorio = numeroAleatorio();
-            empezarJuego();
-        } else {
-            document.write("<h2>El juego se ha cancelado.</h2>");
-        }
+            if (intentos == 1) {
+                document.write("<h2>¡Enhorabuena! Lo has adivinado en " + intentos + " intento.</h2>");
+            } else {
+                document.write("<h2>¡Enhorabuena! Lo has adivinado en " + intentos + " intentos.</h2>");
+            }                       
+            
+        setTimeout(() => {
+            let repetir = confirm("¿Quieres volver a jugar?");
+            if (repetir) {
+                intentos = 0;
+                returnAleatorio = numeroAleatorio();
+                empezarJuego();
+            } else {
+                document.write("<h2>El juego se ha cancelado.</h2>");
+            }
+        }, 500);
+
         } else if (numeroUsuario > returnAleatorio) {            
             alert("El número a adivinar es menor.");
             return empezarJuego();
