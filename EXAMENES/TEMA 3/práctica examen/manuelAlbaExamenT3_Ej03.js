3196dbbe1e98cc09000302ced0ad4f040e8874f8
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-20 18:35:20 +0100

    practicar examen

---
+++ EXAMENES/TEMA 3/práctica examen/manuelAlbaExamenT3_Ej03.js
@@ -0,0 +1,10 @@
+/*Implementa un programa en JavaScript que:
+
+- Divida una cadena de texto en palabras.
+- Muestre en consola:
+- La cantidad de palabras en la cadena.
+- La primera y la última palabra.
+- Las palabras en orden inverso.
+- Las palabras ordenadas alfabéticamente.
+- Las palabras ordenadas alfabéticamente pero en orden inverso. */
+
