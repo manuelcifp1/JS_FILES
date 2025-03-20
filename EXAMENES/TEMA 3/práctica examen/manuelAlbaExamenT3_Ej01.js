c1d2a496160336f748992ce53d19ae0438ca05f5
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-03-20 19:24:24 +0100

    practicando examen

--- EXAMENES/TEMA 3/práctica examen/manuelAlbaExamenT3_Ej01.js
+++
@@ -1,6 +0,0 @@
-/*Crea una función en JavaScript que convierta una cadena de texto en un estilo "Cani", aplicando las siguientes reglas:
-
-- Alternar entre mayúsculas y minúsculas en los caracteres de la cadena.
-- Convertir las letras "c" en "k" y viceversa.
-- Si la última letra de la cadena es una vocal, añadir "HHH" al final del texto. */
-
