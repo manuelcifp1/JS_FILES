cd481d19636a13caee68b33ce36e2015dd90db21
manuelcifp1 <alba_rios_manuel@hotmail.com>
2025-05-21 22:14:46 +0200

    ea

--- PRÁCTICAS/TEMA 8/PRUEBAS/P17/script.js
+++ PRÁCTICAS/TEMA 8/PRUEBAS/P17/script.js
@@ -1,12 +1,14 @@
 export function temporizador(segundos) {
+    //Primero la promesa.
     return new Promise((resolve, reject) => {
-        
+        //Seleccionar el botón.
         const boton = document.getElementById("abortar");
-
+        //El timeOut con su resolve.
         const timeOut = setTimeout(() => {
         resolve("Tiempo concluido");
         }, 1000 * segundos);
 
+        //El botón con su clearTimeout y su reject.
         boton.addEventListener("click", () => {
             clearTimeout(timeOut);
             reject("Acción interrumpida por el usuario");
